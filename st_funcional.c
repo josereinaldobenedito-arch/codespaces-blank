29784d106e10d601008442d6333d0504291e24c7
josereinaldobenedito-arch <josereinaldobenedito@gmail.com>
2026-02-24 06:46:20 -0300

    Atualizar o st_funcional.c

--- st_funcional.c
+++ st_funcional.c
@@ -2,17 +2,31 @@
 
 int main () {
 
+//menu de opções
+
   int opcoes;
   int c1, c2;
-  
-  printf ("****SUPER TRUNFO*****");
-  printf ("iniciarjogo\n");
-  printf ("regrasdoJogo/n")
+
+//propriedades da carta
+
     int c1_população = 3644;
-    int c2_população = 3656;
     float c1_area = 367832;
-    float c2_area = 35984;
     float c1_pib = 98959.65;
-    float c2_pib = 311548.78;
     int c1_pt = 154;
+    
+    int c2_população = 3656;
+    float c2_area = 35984;
+    float c2_pib = 311548.78;
     int c2_pt = 230;
+
+  printf ("****SUPER TRUNFO*****");
+  printf ("1- iniciarjogo\n");
+  printf ("2- regrasdoJogo\n");
+  printf ("3. sair\n");
+  scanf  ("%d", &opcao");
+
+  switch (opcao)
+{
+  case 1:
+  printf ("digite a propriedade a ser comparada:\n");
+scanf ("
