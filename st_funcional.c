bd5c7f1d216d6b25aaa79bb83795d0ea3983f5f3
josereinaldobenedito-arch <josereinaldobenedito@gmail.com>
2026-02-24 06:54:16 -0300

    Atualizar o st_funcional.c

--- st_funcional.c
+++ st_funcional.c
@@ -9,12 +9,12 @@ int main () {
 
 //propriedades da carta
 
-    int c1_população = 3644;
+    int c1_populacao = 3644;
     float c1_area = 367832;
     float c1_pib = 98959.65;
-    int c1_pt = 154;
+    int c1_pt = 230;
     
-    int c2_população = 3656;
+    int c2_populacao = 3656;
     float c2_area = 35984;
     float c2_pib = 311548.78;
     int c2_pt = 230;
@@ -28,5 +28,28 @@ int main () {
   switch (opcao)
 {
   case 1:
-  printf ("digite a propriedade a ser comparada:\n");
-scanf ("
+  if(c1_população > c2_população){
+        printf("carta 1 contém mais população! \n");
+    } else if
+        printf("carta 2 contém mais população! \n");
+    } else {
+         printf ("empate\\n");
+    }
+
+    if(c1_area > c2_area){
+        printf("Carta 1 contém a maior Área! \n");
+    } else {
+        printf("Carta 2 contém a maior Área! \n");
+    }
+
+    if(c1_pib > c2_pib){
+        printf("Carta 1 possui o maior P.I.B.! \n");
+    } else {
+        printf("Carta 2 possui o maior P.I.B.! \n");
+    }
+
+    if(c1_pt > c2_pt){
+        printf("Carta 1 tem mais Pontos Turísticos! \n");
+    } else {
+        printf("Carta 2 tem mais Pontos Turísticos! \n");
+    }
