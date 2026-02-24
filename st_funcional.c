2b2c021ce9ae296a564ae6bce9d2a51e1e742e02
josereinaldobenedito-arch <josereinaldobenedito@gmail.com>
2026-02-24 00:03:15 -0300

    Criar st_funcional.c

---
+++ st_funcional.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+
+int main () {
+
+  int opcoes;
+  int c1, c2;
+  
+  printf ("****SUPER TRUNFO*****");
+  printf ("iniciarjogo\n");
+  printf ("regrasdoJogo/n")
+    int c1_população = 3644;
+    int c2_população = 3656;
+    float c1_area = 367832;
+    float c2_area = 35984;
+    float c1_pib = 98959.65;
+    float c2_pib = 311548.78;
+    int c1_pt = 154;
+    int c2_pt = 230;
